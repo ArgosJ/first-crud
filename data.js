db97a333b48e05f0e5f12fe709f93b9e322a2d35
Jackson Yaipen <yaipen2002@gmail.com>
2021-09-28 05:18:13 -0500

    I uploaded changes

---
+++ data.js
@@ -0,0 +1,19 @@
+let todo=[
+    {
+        id:1,
+        content:"buy groceries"
+    },
+    {
+        id:2,
+        content:"Learn programing"
+    },
+    {
+        id:3,
+        content:"Pay the bills"
+    },
+];
+module.exports = function(){
+    return{
+        todo: todo,
+    };
+};
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col 12">
+        <table class="table table-bordered mt5">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Content</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, i) in todoList" :key="i">
+              <td class="table-danger">{{ item.id }}</td>
+              <td>{{ item.content }}</td>
+              <td class="align-middle text-center w-75">
+                <div class="d-flex">
+                  <button
+                    class="btn btn-info btn-sm mx-1"
+                    @click="handleEdit(item.id)"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    class="btn btn-danger btn-sm mx-1"
+                    @click="handleDelete(item.id)"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="row">
+          <div class="col-6 align-middle text-center w-75">
+            <div class="form-group">
+              <label for="">{{ editMode ? "Edit" : "Add" }}</label>
+              <input
+                type="text"
+                class="form-control"
+                v-model="todoItem.content"
+              />
+            </div>
+          </div>
+          <div class="col-6 align-middle text-center w-25">
+            <button class="btn btn-primary btn-sm mx-1" @click="handleToDoItem">
+              {{ editMode ? "Edit" : "Add" }}
+            </button>
+
+            <button
+              v-if="editMode"
+              class="btn btn-danger btn-sm mx-1"
+              @click="handleCancel"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Axios from "axios";
+const todoUrl = "http://localhost:3500/todo";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "MainComponent",
+  data() {
+    return {
+      todoList: [],
+      todoItem: {},
+      editMode: false
+    };
+  },
+  async created() {
+    const res = await Axios.get(todoUrl);
+    this.todoList = res.data;
+  },
+  methods: {
+    edit() {},
+    handleEdit(id) {
+      this.editMode = true;
+      const item = this.todoList.find(i => i.id === id);
+      this.todoItem = item;
+    },
+    handleDelete(id) {
+      const index = this.todoList.findIndex(i => i.id === id);
+      this.todoList.splice(index, 1);
+    },
+    handleToDoItem() {
+      if (this.editMode === true) {
+        console.log('aqui se edita');
+      } else {
+        this.todoList.push({
+          id: this.todoList.length + 1,
+          content: this.todoItem.content
+        });
+        this.todoItem.content = "";
+      }
+    },
+    handleCancel() {
+      this.editMode = false
+      this.todoItem = {}
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.table > :not(caption) > * > * {
+  background-color: none;
 }
 </style>
